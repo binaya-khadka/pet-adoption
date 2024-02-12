64669d250fd01ddd6528e73b9505b93e6278fe25
binaya-khadka <binayamm@gmail.com>
2024-02-12 22:06:04 +0545

    test

--- client/src/app/screens/about/About.tsx
+++ client/src/app/screens/about/About.tsx
@@ -1,9 +1,27 @@
 import Layout from "../Layout/Layout";
 
 export default function About() {
-  return(
+  return (
     <Layout>
-      <div>ABOUT PAGE</div>
+      <div className="section hero" style={{height: '92dvh'}}>
+        <div className="section-inner">
+          <div className="section-content">
+            <div className="hero-content">
+              <h1 className="hero-title">
+                About Us
+              </h1>
+              <div className="hero-subtitle">
+                This is the website I've created using MERN Stack which contains Mongodb as the database, Express as framework of Node.js,
+                React as the frontend library and Node.js as the backend server.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{background: '#dadada', height: '92dvh'}}>
+        <h1 className="hero-title" style={{
+        }}>About Us</h1>
+      </div> */}
     </Layout>
   )
 }
--- client/src/app/screens/home/Home.tsx
+++ client/src/app/screens/home/Home.tsx
@@ -3,9 +3,10 @@ import * as styles from './home.styles'
 import { useQuery } from 'react-query'
 import { petService } from '../../services'
 import styled from 'styled-components'
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  
+
   const { data, isLoading, isError } = useQuery('home', petService.getAllPets);
 
   return (
@@ -26,23 +27,25 @@ export default function Home() {
               </div>
             </> : <>
               <PetContainer>
-                { data && data?.data?.map(pet => (
-                  <Pet key={pet.id}>
-                    <div style={styles.imageContainer}>
-                      <img style={{
-                        ...styles.img
-                      }} src={`http://localhost:3000/uploads/${pet.image}`} alt={pet.name} />
-                    </div>
-                    <div style={styles.petName}>
-                      {pet.name}
-                    </div>
-                    <div style={styles.petAge}>
-                      {pet.age}
-                    </div>
-                    <div style={styles.petBreed}>
-                      {pet.breed}
-                    </div>
-                  </Pet>
+                {data && data?.data?.map(pet => (
+                  // <Link style={{ textDecoration: 'none' }} to={"/addpet"}>
+                    <Pet key={pet.id}>
+                      <div style={styles.imageContainer}>
+                        <img style={{
+                          ...styles.img
+                        }} src={`http://localhost:3000/uploads/${pet.image}`} alt={pet.name} />
+                      </div>
+                      <div style={styles.petName}>
+                        {pet.name}
+                      </div>
+                      <div style={styles.petAge}>
+                        {pet.age}
+                      </div>
+                      <div style={styles.petBreed}>
+                        {pet.breed}
+                      </div>
+                    </Pet>
+                  // </Link>
                 ))}
               </PetContainer>
             </>
