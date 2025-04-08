3b72bc178f3a961c9e9dd9ef74ee400cac38d6ca
Binaya Khadka <binayamm@gmail.com>
2025-04-08 12:56:22 +0545

    separating nav components

--- client/src/app/components/nav/nav.tsx
+++ client/src/app/components/nav/nav.tsx
@@ -1,38 +1,15 @@
-import { useState, useEffect } from 'react';
-import { getCurrentUser } from '../../store';
-import { User } from '../../../interfaces';
-import { localStorageUtils } from '../../utils';
-import { storageConstants } from '../../../constants';
-import { useNavigate } from 'react-router-dom';
+import useNav from './useNav';
 import styled from 'styled-components';
 
 export const Nav = () => {
-  const navigate = useNavigate();
-
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-
-  const [currentUser, setCurrentUser] = useState<User | null>();
-
-  const [userFetched, setUserFetched] = useState<boolean>(false);
-
-  const logoutUser = () => {
-    localStorageUtils.removeItem(storageConstants.sessionKey);
-    localStorageUtils.removeItem(storageConstants.localUserKey);
-    navigate('/login');
-  };
-
-  useEffect(() => {
-    const fetchedUser = getCurrentUser();
-    if (fetchedUser?.user?.id) {
-      //
-      setCurrentUser(fetchedUser?.user);
-    }
-    setUserFetched(true);
-  }, []);
-
-  const onClickLink = () => {
-    setShowMenu(false);
-  };
+  const {
+    showMenu,
+    currentUser,
+    userFetched,
+    logoutUser,
+    setShowMenu,
+    onClickLink
+  } = useNav();
 
   return (
     <>
