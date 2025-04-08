bcc97ab6d41aa6384689069c2fbe0f4c7abeaf91
Binaya Khadka <binayamm@gmail.com>
2025-04-08 07:54:20 +0545

    apimethods types created

---
+++ server/src/utils/apiMethod.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+
+interface ApiResponse {
+  req: Request,
+  res: Response,
+  message?: string,
+  status: {
+    code: number,
+    success: boolean,
+  }
+}
+
+interface ApiSuccess extends ApiResponse {
+  data?: any,
+}
+
+interface ApiFail extends ApiResponse {
+  error?: any,
+}
+
+const apiSuccess = ({ req, res, data, message = 'Success',
+  status = {
+    code: 200,
+    success: true,
+  },
+}: ApiSuccess) => {
+  console.log({
+    message,
+    method: req?.method,
+    route: req?.url,
+  })
+  return res.status(status.code || 200).json({ message, status, data })
+}
+
+const apiFail = ({ req, res, error, message = 'Error Occured',
+  status = {
+    code: 400,
+    success: false,
+  },
+}: ApiFail) => {
+  console.log(error);
+  return res.status(status.code || 400).json({
+    message,
+    status: {
+      code: status.code || 400,
+      success: status.success || false,
+    },
+    error,
+  })
+}
+
+export { apiSuccess, apiFail }
