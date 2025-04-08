31554557facae879d111cf84349f7b251ff85f92
Binaya Khadka <binayamm@gmail.com>
2025-04-08 07:46:11 +0545

    added values to config file

--- server/src/lib/config.ts
+++ server/src/lib/config.ts
@@ -10,6 +10,9 @@ function loadFromEnv(key: string): string {
 
 const config = {
     mongoDBUrl: loadFromEnv("MONGO_URI"),
+    totalValidationDays: parseInt(loadFromEnv('TOTAL_VALIDATION_DAYS')) || 250,
+    port: parseInt(loadFromEnv('PORT')) || 3000,
+    jwtSecret: loadFromEnv("JWT_SECRET"),
 };
 
 export { config as serverConfig };
