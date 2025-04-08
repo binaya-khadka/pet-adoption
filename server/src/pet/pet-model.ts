12e18de524c75463d3484b6f52b9392e4d5448b2
Binaya Khadka <binayamm@gmail.com>
2025-04-08 13:38:21 +0545

    formatting pet schema

--- server/src/pet/pet-model.ts
+++ server/src/pet/pet-model.ts
@@ -2,37 +2,14 @@ import { Pet } from 'src/types';
 import mongoose, { Model, mongo, Schema } from 'mongoose';
 
 const PetSchema = new Schema<Pet>({
-  name: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String
-  },
-  age: {
-    type: String,
-    required: true
-  },
-  breed: {
-    type: String,
-    required: true
-  },
-  isAdopted: {
-    type: Boolean,
-    default: false
-  },
-  onAdoptionByUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  adoptedByUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  name: { type: String, required: true },
+  image: { type: String },
+  age: { type: String, required: true },
+  breed: { type: String, required: true },
+  isAdopted: { type: Boolean, default: false },
+  onAdoptionByUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  adoptedByUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
 });
 
 const Pets =
